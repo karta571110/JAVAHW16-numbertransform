f687ef73b0b57813c72ac4191bfd561eebaf679e
karta571110 <karta571110@gmail.com>
2019-10-30 18:38:41 +0800

    尚未測試

--- JAVA/src/main/java/com/example/java/MyClass.java
+++ JAVA/src/main/java/com/example/java/MyClass.java
@@ -6,11 +6,30 @@ public class MyClass {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            System.out.println("½Ð¿é¤Jªü©Ô§B¼Æ¦r¥H¨ÑÂà´«:");
             String in = scanner.nextLine();
             for (int i = 0; i < in.length(); i++) {
                 switch (Integer.parseInt(String.valueOf(in.charAt(i)))) {
                     case 0:
-                        System.out.print("é›¶");
+                        System.out.print("¹s");
+                    case 1:
+                        System.out.print("³ü");
+                    case 2:
+                        System.out.print("¶L");
+                    case 3:
+                        System.out.print("°Ñ");
+                    case 4:
+                        System.out.print("¸v");
+                    case 5:
+                        System.out.print("¥î");
+                    case 6:
+                        System.out.print("³°");
+                    case 7:
+                        System.out.print("¬m");
+                    case 8:
+                        System.out.print("®Ã");
+                    case 9:
+                        System.out.print("¨h");
                 }
 
             }
